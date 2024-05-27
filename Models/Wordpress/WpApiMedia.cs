dc3c7204f5c3d43e605e661193c781219b62630e
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-05-28 01:37:10 +0200

    Show top image

---
+++ Models/Wordpress/WpApiMedia.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using MichaelKjellander.Utils;
+using Microsoft.AspNetCore.Authentication;
+
+namespace MichaelKjellander.Models.Wordpress;
+
+public class WpApiMedia : IParsableJson
+{
+    public int Id { get; private init; }
+    public string? PostThumbnailUrl { get; private init; }
+
+    public static IParsableJson ParseFromJson(JsonElement el)
+    {
+        var id = el.GetProperty("id").GetInt32();
+        var postThumbnailUrl = el.GetProperty("media_details").GetProperty("sizes").GetProperty("post-thumbnail")
+            .GetProperty("source_url")
+            .GetString();
+
+        return new WpApiMedia { Id = id, PostThumbnailUrl = postThumbnailUrl };
+    }
+}
--- Models/Wordpress/WpApiPost.cs
+++ Models/Wordpress/WpApiPost.cs
@@ -8,15 +8,21 @@ public class WpApiPost : IParsableJson
     public int Id { get; private init; }
     public string Content { get; private init; }
     public string? Title { get; private init; }
-    public DateOnly Date { get; private init;  }
+    public DateOnly Date { get; private init; }
     public int CategoryId { get; private init; }
+    public int FeaturedMediaId { get; private init; }
     public WpApiCategory? Category { get; private set; }
+    public WpApiMedia? FeaturedMedia { get; private set; }
 
     public void FindAndSetCategory(ICollection<WpApiCategory> categories)
     {
         this.Category = categories.First(c => c.Id == this.CategoryId);
     }
-    
+    public void FindAndSetFeaturedMedia(ICollection<WpApiMedia> medias)
+    {
+        this.FeaturedMedia = medias.FirstOrDefault(m => m.Id == this.FeaturedMediaId);
+    }
+
 
     public static IParsableJson ParseFromJson(JsonElement el)
     {
@@ -25,10 +31,13 @@ public class WpApiPost : IParsableJson
         var title = el.GetProperty("title").GetProperty("rendered").GetString();
         var categoryId = el.GetProperty("categories").EnumerateArray().FirstOrDefault().GetInt32();
         var dateString = el.GetProperty("date").GetString();
-
         var date = DateOnly.Parse(dateString);
-        
-        return new WpApiPost{Id = id, Content = content, Title = title, CategoryId = categoryId, Date = date};
+        var featuredMediaId = el.GetProperty("featured_media").GetInt32();
+
+        return new WpApiPost
+        {
+            Id = id, Content = content, Title = title, CategoryId = categoryId, Date = date,
+            FeaturedMediaId = featuredMediaId
+        };
     }
 }
-
