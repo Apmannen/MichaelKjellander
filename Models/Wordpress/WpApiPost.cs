20939f403aa3dfee1a131d61f9e4ce0017ddb532
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-05-27 18:48:39 +0200

    Temporary parse method

--- Models/Wordpress/WpApiPost.cs
+++ Models/Wordpress/WpApiPost.cs
@@ -1,16 +1,37 @@
+using System.Text.Json;
+
 namespace MichaelKjellander.Models.Wordpress;
 
 public class WpApiPost
 {
-    public int id;
-    public RenderedProp title;
-
-    public int Id => id;
-    public string Title => title.rendered;
-    
+    public int Id { get; set; }
+    public string Title { get; set; }
 }
 
-public class RenderedProp
+public class Tmp
 {
-    public string rendered;
+    //TODO: move
+    //TODO: generalize
+    public static async Task<List<WpApiPost>> ParsePosts(HttpResponseMessage responseMessage)
+    {
+        using var responseStream = await responseMessage.Content.ReadAsStreamAsync();
+        using StreamReader reader = new StreamReader(responseStream);
+
+        string content = reader.ReadToEnd();
+
+        using JsonDocument doc = JsonDocument.Parse(content);
+        JsonElement root = doc.RootElement;
+
+        List<WpApiPost> posts = new();
+        foreach (var el in root.EnumerateArray())
+        {
+            int id = el.GetProperty("id").GetInt32();
+            string title = el.GetProperty("title").GetProperty("rendered").GetString();
+
+            WpApiPost post = new WpApiPost { Id = id, Title = title };
+            posts.Add(post);
+        }
+
+        return posts;
+    }
 }
