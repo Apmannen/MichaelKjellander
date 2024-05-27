2e1579d0c8e3dc9992fd7e721beb2e0f6223eb07
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-05-27 14:02:59 +0200

    Try create API controller

---
+++ Controllers/PingController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MichaelKjellander.Controllers;
+
+[ApiController]
+[Route("api")]
+public class PingController : Controller
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "pong";
+    }
+}
