d2f07cb694e6989ad7dec96a7b458fab84300d32
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-05-28 20:21:10 +0200

    Tag icon

--- Data/WpContext.cs
+++ Data/WpContext.cs
@@ -57,15 +57,3 @@ public class WpContext
         return JsonUtil.ParseList<T>(element);
     }
 }
-
-/*public class BloggingContext : DbContext
-{
-    public DbSet<BlogPost> BlogPosts { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        string connectionString = "server=localhost;user=root;password=;database=kjelledb";
-        MySqlServerVersion serverVersion = new MySqlServerVersion(new Version(8, 3, 0));
-        options.UseMySql(connectionString, serverVersion);
-    }
-}*/
