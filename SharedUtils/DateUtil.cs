a685a921e8a86a3b41b79a071139484e8f2355c0
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-05-31 21:19:58 +0200

    7moDAJkX; Format date

---
+++ SharedUtils/DateUtil.cs
@@ -0,0 +1,9 @@
+﻿namespace MichaelKjellander.SharedUtils;
+
+public static class DateUtil
+{
+    public static string FormatDate(DateOnly date)
+    {
+        return date.ToString("yyyy-MM-dd");
+    }
+}
