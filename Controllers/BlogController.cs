0149090a2f483b94f64f4eef9544213a34290b36
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-01 13:38:40 +0200

    Variable naming conventions

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -10,6 +10,7 @@ namespace MichaelKjellander.Controllers;
 [Route("api/blog")]
 public class BlogController : Controller
 {
+    private const int OneHour = 3600;
     private readonly WpContext _wpContext;
     public BlogController(WpContext wpContext)
     {
@@ -18,7 +19,7 @@ public class BlogController : Controller
     
     [HttpGet]
     [Route("posts")]
-    [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Any, NoStore = false, VaryByQueryKeys = ["Page"])]
+    [ResponseCache(Duration = OneHour, Location = ResponseCacheLocation.Any, NoStore = false, VaryByQueryKeys = ["Page"])]
     public async Task<IActionResult> Get([FromQuery] PostsRequest postsRequest)
     {
         int page = postsRequest.Page ?? 1;
