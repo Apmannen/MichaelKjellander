0cc2c3584e2320c965479ac9db293dcabdfa152a
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-01 19:12:40 +0200

    9PtNo7cl; Category pattern

--- SharedUtils/Routes/PageRoutes.cs
+++ SharedUtils/Routes/PageRoutes.cs
@@ -1,3 +1,5 @@
+using MichaelKjellander.Models.Wordpress;
+
 namespace MichaelKjellander.SharedUtils.Routes;
 
 public static class PageRoutes
@@ -7,6 +9,17 @@ public static class PageRoutes
     public static string Category(string slug)
     {
         return "/kategori/" + slug;
+    }
+
+    public static string Category(CategoryType type)
+    {
+        return Category(WpCategory.GetSlug(type));
+    }
+
+    public static string CategoryPattern(CategoryType type)
+    {
+        string path = Category(type);
+        return $"{path}|{path}/[0-9]+";
     }
 
     public static string Page(string slug)
