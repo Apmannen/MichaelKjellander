ac4cdfe515246a86c6481cd8004bce5a0cc6fbf2
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-02 22:50:11 +0200

    brF0yb5f; Added MySQL connection

---
+++ Data/WebGamesDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace MichaelKjellander.Data;
+
+public class WebGamesDataContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        ServerVersion serverVersion = ServerVersion.Create(8, 4, 0, ServerType.MySql);
+        string connectionString =
+            "server=localhost:3306;database=kjelle_db;user=mickj;password=test;SslMode=none";
+        optionsBuilder.UseMySql(
+            connectionString: connectionString, 
+            serverVersion: serverVersion,
+            mySqlOptionsAction: null);
+    }
+}
---
+++ Models/WebGames/Word.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MichaelKjellander.Models.WebGames;
+
+public class Word
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(50)]
+    [Required]
+    public string? WordString { get; set; }
+}
