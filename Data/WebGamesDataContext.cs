af3270700d0fab7f3e677ad16b8f732e0f50846d
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-02 23:14:49 +0200

    brF0yb5f; Generated DB migration scripts

--- Data/WebGamesDataContext.cs
+++ Data/WebGamesDataContext.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MichaelKjellander.Models.WebGames;
+using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace MichaelKjellander.Data;
 
 public class WebGamesDataContext : DbContext
 {
+    public DbSet<Word> Words { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         ServerVersion serverVersion = ServerVersion.Create(8, 4, 0, ServerType.MySql);
         //TODO: configurable connect string
         string connectionString =
-            "server=localhost:3306;database=kjelle_db;user=mickj;password=test;SslMode=none";
+            "Server=127.0.0.1;Database=kjelle_db;User=root;Password=test;Port=3306;SslMode=none;";
+
         optionsBuilder.UseMySql(
             connectionString: connectionString, 
             serverVersion: serverVersion,
