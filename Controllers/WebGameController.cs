f467fedbe04dccfc39560a8e7a16570fb57c3179
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-03 12:37:25 +0200

    brF0yb5f; Cleanup. Updated route.

--- Controllers/WebGameController.cs
+++ Controllers/WebGameController.cs
@@ -18,40 +18,8 @@ public class WebGameController : Controller
         _logger = logger;
     }
 
-    
     [HttpGet]
-    [Route("random-word")]
-    [System.Obsolete("Not used")]
-    public async Task<IActionResult> GetRandomWord()
-    {
-        await using WebGamesDataContext context = new WebGamesDataContext();
-        Word word = context.Words.FromSqlRaw("SELECT * FROM words w WHERE LENGTH(w.WordString)>=5  ORDER BY RAND() DESC LIMIT 1")
-            .FirstOrDefault();
-
-        return Ok(ApiUtil.CreateApiResponse<Word>([word]));
-    }
-    
-    [HttpGet]
-    [Route("random-words")]
-    [System.Obsolete("Not used")]
-    public async Task<IActionResult> GetWords()
-    {
-        await using WebGamesDataContext context = new WebGamesDataContext();
-        List<Word> words = await context.Words.Where(row => row.WordString.Length >= 5).ToListAsync();
-        CollectionUtil.Shuffle(words);
-
-        List<Word> filteredWords = [];
-        for (int i = 0; i < 1000; i++)
-        {
-            filteredWords.Add(words[i]);
-        }
-        filteredWords.Sort((word1, word2) => ((int)word1.Id!).CompareTo((int)word2.Id!));
-
-        return Ok(ApiUtil.CreateApiResponse(filteredWords));
-    }
-
-    [HttpGet]
-    [Route("init-guess-words-game")]
+    [Route("word-guess/init")]
     public async Task<IActionResult> InitGame()
     {
         _logger.LogInformation("**** Initing game!");
