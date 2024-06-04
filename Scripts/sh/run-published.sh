51ffabee4c220df29a823b3bb72ebf25111c93d9
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-05 00:46:50 +0200

    Cleanup

--- Scripts/sh/run-published.sh
+++ Scripts/sh/run-published.sh
@@ -1,2 +1,2 @@
 ﻿cd /root/gits/MichaelKjellander/bin/Release/net8.0/publish
-ASPNETCORE_URLS="https://new.michaelkjellander.se;https://95.141.241.213:443;http://95.141.241.213:80" SG_APPENVIRONMENT=WwwDev dotnet MichaelKjellander.dll
+ASPNETCORE_URLS="https://new.michaelkjellander.se" SG_APPENVIRONMENT=WwwDev dotnet MichaelKjellander.dll
