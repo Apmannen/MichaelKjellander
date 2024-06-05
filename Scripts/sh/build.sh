5e38ab4cbc4081c8a95e741310c4999ec19e2215
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-05 19:43:12 +0200

    7; Update build(/publish) script to compile published styles

--- Scripts/sh/build.sh
+++ Scripts/sh/build.sh
@@ -1,3 +1,4 @@
 /root/.dotnet/dotnet publish
+npx tailwindcss -i bin/Release/net8.0/publish/wwwroot/MichaelKjellander.styles.css -o bin/Release/net8.0/publish/wwwroot/components.compiled.css
 cp -r /root/gits/MichaelKjellander/bin/Release/net8.0/publish/* /opt/myapp/
 chown -R www-data:www-data /opt/myapp
