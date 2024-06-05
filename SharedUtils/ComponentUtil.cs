4821dfe4501ea4409d27a68d1433e2612e7a5985
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-05 18:34:15 +0200

    #1; Single post page

---
+++ SharedUtils/ComponentUtil.cs
@@ -0,0 +1,9 @@
+namespace MichaelKjellander.SharedUtils;
+
+public static class ComponentUtil
+{
+    public static string FormatPageTitle(string pageName)
+    {
+        return $"{pageName} - Michael Kjellander";
+    }
+}
