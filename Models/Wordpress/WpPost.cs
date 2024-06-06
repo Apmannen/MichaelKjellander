6997d599ba2b02956371a909ec3a896d9c43efac
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-06 04:30:06 +0200

    #12; Major cleanup

--- Models/Wordpress/WpPost.cs
+++ Models/Wordpress/WpPost.cs
@@ -9,13 +9,8 @@ public class WpPost : Model
     public string? Title { get; set; }
     public string? Slug { get; set; }
     public DateOnly Date { get; set; }
-    //public int CategoryId { get; set; }
-    //public int FeaturedMediaId { get; set; }
     public WpCategory? Category { get; set; }
     public string? FeaturedMediaUrl { get; set; }
-    //public WpMedia? FeaturedMedia { get; set; }
-    //public IList<int>? TagIds { get; set;  }
-    //public IList<WpTag>? Tags { get; set;  }
     public IList<string>? MetaPlatforms { get; set; }
     public string? MetaPlayAlso { get; set; }
     public int? MetaRating { get; set; }
@@ -42,19 +37,6 @@ public class WpPost : Model
             };
         }
     }
-
-    // public void FindAndSetCategory(ICollection<WpCategory> categories)
-    // {
-    //     this.Category = categories.First(c => c.Id == this.CategoryId);
-    // }
-    // public void FindAndSetFeaturedMedia(ICollection<WpMedia> medias)
-    // {
-    //     this.FeaturedMedia = medias.FirstOrDefault(m => m.Id == this.FeaturedMediaId);
-    // }
-    // public void FindAndSetTags(ICollection<WpTag> tags)
-    // {
-    //     this.Tags = tags.Where(t => this.TagIds!.Contains(t.Id)).ToArray();
-    // }
     
     public override void ParseFromJson(JsonElement el)
     {
@@ -67,12 +49,6 @@ public class WpPost : Model
         dateString = dateString.Replace(' ', 'T');
         DateOnly date = DateOnly.Parse(dateString);
         string? featuredMediaUrl = el.GetProperty("featured_image").GetString();
-        //var tagIdsEnumerator = el.GetProperty("tags").EnumerateArray();
-        // List<int> tagIds = new List<int>();
-        // foreach(JsonElement tagEl in tagIdsEnumerator)
-        // {
-        //     tagIds.Add(tagEl.GetInt32());
-        // }
         var metaElement = el.GetProperty("meta");
         
         this.Id = id;
@@ -80,11 +56,9 @@ public class WpPost : Model
         this.Title = title;
         this.Slug = slug;
         this.Date = date;
-        //this.CategoryId = categoryId;
         this.Category = new WpCategory();
         this.Category.ParseFromJson(el.GetProperty("category"));
         this.FeaturedMediaUrl = featuredMediaUrl;
-        //this.TagIds = tagIds;
         this.MetaPlatforms = TryParseStrings(metaElement, "format");
         this.MetaPlayAlso = TryParseString(metaElement, "play_also");
         this.MetaRating = TryParseInt(metaElement, "rating");
--- Services/WpApiService.cs
+++ Services/WpApiService.cs
@@ -14,75 +14,27 @@ public class WpApiService
     {
         this._client = client;
     }
-
-
+    
     public async Task<WpPage?> GetPage(string slug = "")
     {
-        //var pagesResult = await FetchAndParseFromWpApiWithHeaders<WpPage>($"pages?slug={slug}");
         var pagesResult = await ApiUtil.FetchJson(CreateRequestUrl($"pages?slug={slug}"), _client);
         IList<WpPage> parsedPages = Model.ParseList<WpPage>(pagesResult.Root);
-        //ICollection<WpPage> pages = pagesResult.ParsedElements;
         return parsedPages.FirstOrDefault();
     }
     
     //TODO: use some query builder, #16
+    //TODO: multiple return values isn't the best
     public async Task<(IList<WpPost>,int)> GetPosts(int page = 1, string? categorySlug = null, string? postSlug = null)
     {
-        //ICollection<WpCategory>? categories = null;
         string postsFetchString = $"posts?page={page}";
-
         if (postSlug != null)
         {
             postsFetchString += $"&slug={postSlug}";
         }
-
         if (categorySlug != null)
         {
             postsFetchString += $"&category_slug={categorySlug}";
         }
-        // if (categorySlug != null)
-        // {
-        //     var categoriesResult = await FetchAndParseFromWpApiWithHeaders<WpCategory>($"categories?slug={categorySlug}");
-        //     categories = categoriesResult.ParsedElements;
-        //     WpCategory? category = categories.FirstOrDefault();
-        //     if (category == null)
-        //     {
-        //         return ([], 0);
-        //     }
-        //     postsFetchString += $"&categories={category.Id}";
-        // }
-        
-        //var postsResult = await FetchAndParseFromWpApiWithHeaders<WpPost>(postsFetchString);
-        //IList<WpPost> posts = postsResult.ParsedElements;
-        //int numPages = int.Parse(postsResult.Headers.GetValues("X-WP-TotalPages").First());
-        
-        // var mediaIds = new HashSet<int>();
-        // var tagIds = new HashSet<int>();
-        // var categoryIds = new HashSet<int>();
-        // foreach (WpPost post in posts)
-        // {
-        //     if (post.FeaturedMediaId != 0)
-        //     {
-        //         mediaIds.Add(post.FeaturedMediaId);
-        //     }
-        //     foreach (int tagId in post.TagIds!)
-        //     {
-        //         tagIds.Add(tagId);
-        //     }
-        //     categoryIds.Add(post.CategoryId);
-        // }
-        //
-        // ICollection<WpMedia> medias = await FetchAndParseExtras<WpMedia>("media", mediaIds);
-        // ICollection<WpTag> tags = await FetchAndParseExtras<WpTag>("tags", tagIds);
-        // categories = categories ?? await FetchAndParseExtras<WpCategory>("categories", categoryIds);
-
-        // foreach (WpPost post in posts)
-        // {
-        //     post.FindAndSetCategory(categories);
-        //     post.FindAndSetFeaturedMedia(medias);
-        //     post.FindAndSetTags(tags);
-        // }
-
         string fullUrl = CreateRequestUrl(postsFetchString, "sgplugin/v1");
         var postsResult = await ApiUtil.FetchJson(fullUrl, _client);
         JsonElement root = postsResult.Root;
@@ -92,41 +44,8 @@ public class WpApiService
         return (parsedPosts, numPages);
     }
 
-    private string CreateRequestUrl(string uri, string nameSpace = "wp/v2")
+    private static string CreateRequestUrl(string uri, string nameSpace = "wp/v2")
     {
         return $"https://michaelkjellander.se/wp-json/{nameSpace}/{uri}";
     }
-
-    // private async Task<ICollection<T>> FetchAndParseExtras<T>(string type, ICollection<int> ids) where T : Model
-    // {
-    //     if (ids.Count == 0)
-    //     {
-    //         return [];
-    //     }
-    //     string idsString = string.Join(",", ids);
-    //     ICollection<T> result = await FetchAndParseFromWpApi<T>($"{type}?include={idsString}");
-    //     return result;
-    // }
-
-    // private async Task<ICollection<T>> FetchAndParseFromWpApi<T>(string uri)
-    //     where T : Model
-    // {
-    //     JsonFetchElementsResult<T> result = await FetchAndParseFromWpApiWithHeaders<T>(uri);
-    //     return result.ParsedElements;
-    // }
-    //
-    // private async Task<JsonFetchElementsResult<T>> FetchAndParseFromWpApiWithHeaders<T>(string uri, string nameSpace = "wp/v2")
-    //     where T : Model
-    // {
-    //     JsonFetchResult result = await ApiUtil.FetchJson($"https://michaelkjellander.se/wp-json/{nameSpace}/{uri}", _client);
-    //     IList<T> parsedElements = Model.ParseList<T>(result.Root);
-    //     return new JsonFetchElementsResult<T>(parsedElements, result.Headers);
-    // }
-    //
-    // //TODO: record?
-    // private struct JsonFetchElementsResult<T>(IList<T> parsedElements, HttpResponseHeaders headers) where T : Model
-    // {
-    //     public readonly IList<T> ParsedElements = parsedElements;
-    //     public readonly HttpResponseHeaders Headers = headers;
-    // }
 }
