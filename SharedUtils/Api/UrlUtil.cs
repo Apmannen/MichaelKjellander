1d7f2d2fbd5aa6169d14652be65361c70f8b7bbe
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-06 21:51:01 +0200

    #12; Generalized URL utils

---
+++ SharedUtils/Api/UrlUtil.cs
@@ -0,0 +1,30 @@
+﻿namespace MichaelKjellander.SharedUtils.Api;
+
+public class UrlUtil
+{
+    public static string GetQueryParameter(string url, string name)
+    {
+        var uriBuilder = new UriBuilder(url);
+        var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+        return query[name] ?? "";
+    }
+    
+    public static ICollection<string> GetQueryParameters(string url, string name)
+    {
+        return GetQueryParameter(url, name).Split(",");
+    }
+    public static ICollection<int> GetQueryParametersInt(string url, string name)
+    {
+        ICollection<string> stringValues = GetQueryParameter(url, name).Split(",");
+        List<int> intValues = new();
+        foreach (string stringValue in stringValues)
+        {
+            bool isValid = int.TryParse(stringValue, out int parsedValue);
+            if (isValid)
+            {
+                intValues.Add(parsedValue);
+            }
+        }
+        return intValues;
+    }
+}
--- SharedUtils/ComponentUtil.cs
+++ SharedUtils/ComponentUtil.cs
@@ -20,30 +20,4 @@ public static class ComponentUtil
         
         return title;
     }
-    
-    public static string GetQueryParameter(NavigationManager navigationManager, string name)
-    {
-        var uriBuilder = new UriBuilder(navigationManager.Uri);
-        var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
-        return query[name] ?? "";
-    }
-    
-    public static ICollection<string> GetQueryParameters(NavigationManager navigationManager, string name)
-    {
-        return GetQueryParameter(navigationManager, name).Split(",");
-    }
-    public static ICollection<int> GetQueryParametersInt(NavigationManager navigationManager, string name)
-    {
-        ICollection<string> stringValues = GetQueryParameter(navigationManager, name).Split(",");
-        List<int> intValues = new();
-        foreach (string stringValue in stringValues)
-        {
-            bool isValid = int.TryParse(stringValue, out int parsedValue);
-            if (isValid)
-            {
-                intValues.Add(parsedValue);
-            }
-        }
-        return intValues;
-    }
 }
