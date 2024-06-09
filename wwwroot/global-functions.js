ec75cc98479706130b13ff75ccd982e3ec7f2bc0
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-09 18:07:24 +0200

    #10; Cleanup

--- wwwroot/global-functions.js
+++ wwwroot/global-functions.js
@@ -1,16 +1,18 @@
 ﻿window.clickResults = {};
 
-window.attachExclusiveListener = function (id, callback) {
+window.attachExclusiveListener = function (id) {
+    //console.log("*** attachExclusiveListener", id)
     const target = document.getElementById(id);
+    //console.log("*** attach to target", target)
     target.addEventListener("click", (event) => {
         const isTarget = event.target === target;
-        console.log("**** isTarget", isTarget)
+        //console.log("**** isTarget", isTarget)
         window.clickResults[id] = isTarget;
     });
 }
 
 window.getClickResult = function (id) {
-    console.log("**** clickresults", window.clickResults)
+    //console.log("**** clickresults", window.clickResults)
     const wasTarget = window.clickResults[id] === true;
     window.clickResults[id] = undefined;
     return wasTarget;
