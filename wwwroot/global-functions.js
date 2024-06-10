c20ca9cfc2676d6212dc5246c6def9b2e17a3e55
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-10 21:21:45 +0200

    #10; Full size images in modals

--- wwwroot/global-functions.js
+++ wwwroot/global-functions.js
@@ -5,8 +5,13 @@
         image.addEventListener("click", (event) => {
             event.preventDefault();
             targetElement.setAttribute("data-active", true);
-            contentContainer.innerHTML = "";
-            contentContainer.appendChild(image.cloneNode());
+            const src = image.getAttribute("src");
+            const splits = src.split(/-[0-9]+x[0-9]+/);
+            let fullSizeSrc = src;
+            if(splits.length === 2) {
+                fullSizeSrc = splits[0]+splits[1];
+            }
+            contentContainer.innerHTML = "<img src='"+fullSizeSrc+"'>";
             
             targetElement.addEventListener("click", (event) => {
                 if(event.target === targetElement) {
