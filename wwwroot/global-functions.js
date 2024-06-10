920f3aa5c54be324a951f7ddaddb21a96b23cb61
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-10 19:59:02 +0200

    #10; Trigger modal on gallery image click

--- wwwroot/global-functions.js
+++ wwwroot/global-functions.js
@@ -1,15 +1,16 @@
-﻿window.trackedClickElement = null;
+﻿
 
 window.trackClickAndActivateTarget = function (classes, targetElement) {
     console.log("**** classes", classes, "target", targetElement);
     for (let c of classes) {
         const anElement = document.getElementsByClassName(c)[0];
         anElement.addEventListener("click", (event) => {
-            console.log("**** el click", this)
+            targetElement.setAttribute("data-active", true);
         });
     }
 }
 
+/*window.trackedClickElement = null;
 window.attachExclusiveListener = function (target) {
     target.addEventListener("click", (event) => {
         window.trackedClickElement = event.target;
@@ -17,7 +18,7 @@ window.attachExclusiveListener = function (target) {
 }
 window.checkIfTargetHasBeenClicked = function (target) {
     return window.trackedClickElement === target;
-}
+}*/
 
 window.debugObject = function (obj) {
     console.log(obj);
