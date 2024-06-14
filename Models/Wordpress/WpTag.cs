fe7a9b1e5f1d5d43d4cf8338487248ad891dfe6f
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-15 01:11:36 +0200

    #51; Prepared tag model

---
+++ Models/Wordpress/WpTag.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace MichaelKjellander.Models.Wordpress;
+
+public class WpTag : WordpressModel
+{
+    public string? Name { get; set; }
+    public string? Slug { get; set; }
+    
+    public override WpTag ParseFromJson(JsonElement el)
+    {
+        Id = el.GetProperty("term_id").GetInt32();
+        Name = el.GetProperty("name").GetString();
+        Slug = el.GetProperty("slug").GetString();
+
+        return this;
+    }
+}
