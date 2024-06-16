dd484649dc854c1a943e3c5289dfae2a7d8ea3ee
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-16 20:23:21 +0200

    #26; Prepared translation entry model

---
+++ Models/Wordpress/WpTranslationEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MichaelKjellander.Models.Wordpress;
+
+public enum Language
+{
+    Swedish = 0
+}
+
+public class WpTranslationEntry : DbModel
+{
+    [Required]
+    [MaxLength(VarcharLength)]
+    public string? Key { get; set; }
+    [Required] 
+    public Language Language { get; set; }
+    [Required] 
+    public string? Translation { get; set; }
+}
