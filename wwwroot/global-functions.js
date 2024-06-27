8bca160d77faf584e67afdf3039c63e606fde453
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-06-27 18:33:05 +0200

    #54; Ignore toggle button clicks while dragging

--- wwwroot/global-functions.js
+++ wwwroot/global-functions.js
@@ -9,27 +9,32 @@ window.attachDraggable = function (draggableParent) {
     let mouseDown = false;
     let startX, scrollLeft;
     const slider = draggableParent;
-    //const debugContainer = document.getElementById("debugcontainer");
-
+    let ignoreMode;
+    const setIgnoreMode = (newIgnoreMode) => {
+        ignoreMode = newIgnoreMode;
+        slider.setAttribute("data-ignore-mode", newIgnoreMode)
+    }
     const getMouseX = (e) => {
         return e.pageX || e.touches[0].pageX;
     }
-
     const startDragging = (e) => {
         mouseDown = true;
         startX = getMouseX(e) - slider.offsetLeft;
         scrollLeft = slider.scrollLeft;
     }
-
     const stopDragging = (e) => {
         mouseDown = false;
+    }    
+    const onMouseDown = (e) => {
+        setIgnoreMode(false);
+        startDragging(e);
     }
-
     const move = (e) => {
         e.preventDefault();
         if (!mouseDown) {
             return;
         }
+        setIgnoreMode(true);
         let mouseX = getMouseX(e);
         const x = mouseX - slider.offsetLeft;
         const scroll = x - startX;
@@ -42,19 +47,22 @@ window.attachDraggable = function (draggableParent) {
         const button = buttonContainer.getElementsByTagName("button")[0];
         const checkBox = buttonContainer.getElementsByTagName("input")[0];
         button.addEventListener("click", function () {
+            if(ignoreMode) {
+                return;
+            }
             checkBox.click();
         });
     }
 
-    slider.even
-
+    setIgnoreMode(false);
+    
     slider.addEventListener('mousemove', move, false);
-    slider.addEventListener('mousedown', startDragging, false);
+    slider.addEventListener('mousedown', onMouseDown, false);
     slider.addEventListener('mouseup', stopDragging, false);
     slider.addEventListener('mouseleave', stopDragging, false);
 
     slider.addEventListener('touchmove', move, false);
-    slider.addEventListener('touchstart', startDragging, false);
+    slider.addEventListener('touchstart', onMouseDown, false);
     slider.addEventListener('touchend', stopDragging, false);
     slider.addEventListener('touchcancel', stopDragging, false);
 }
