a482a8c87bcbf4f83bc531293f4f5b996d44df57
Apmannen <38687594+Apmannen@users.noreply.github.com>
2024-07-02 11:48:50 +0200

    Updated TKey imports

--- Domains/Models/Wordpress/WpPost.cs
+++ Domains/Models/Wordpress/WpPost.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json;
-using MichaelKjellander.Scripts.Startup.Generated;
 
 namespace MichaelKjellander.Domains.Models.Wordpress;
 
